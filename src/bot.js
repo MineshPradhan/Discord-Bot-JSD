71b6c91aa9a1b76e08cae9ca9f89339ed3caddd6
MineshPradhan <mineshpradhan2002@gmail.com>
2022-07-01 13:15:15 +0530

    Intents fixed

---
+++ src/bot.js
@@ -0,0 +1,11 @@
+// require('dotenv').config();
+
+// const { Client, Intents } = require('discord.js');
+
+// const myIntents = new Intents();
+
+// // const {Client} = require('discord.js');
+// // const client = new Client();
+// const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// client.login(process.env.DISCORDJS_BOT_TOKEN);
